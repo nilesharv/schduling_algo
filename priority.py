bdf3ca95f1866a18678413561ede0b1a9a12489f
krishna sinha <nitksinhaji@gmail.com>
2016-09-19 18:43:42 +0530

    second

---
+++ priority.py
@@ -0,0 +1,99 @@
+
+
+#priority algorithm 
+
+class PCB:
+	x=0
+
+	def __init__(self,at,bt,p):
+		self.at=at
+		self.bt=bt
+		self.rt=bt
+		self.p=p
+		self.tt=0
+		self.wt=0
+		self.name='p['+str(PCB.x)+']'
+		PCB.x=PCB.x+1
+	def cal(self,cpu):
+		self.tt=cpu-self.at
+		self.wt=self.tt-self.bt
+		return
+
+	def __str__ (self):
+		return self.name+'\t'+str(self.tt)+'\t'+str(self.wt)+'\t';
+
+
+
+x=int(input('number of process : '))
+process=[]
+final=[]
+wt=0
+tt=0
+remain=x
+sat=-1
+for i in range(0,x):
+	print('p['+str(i)+']')
+	at=int(input('AT: '))
+	bt=int(input('BT: '))
+	p=int(input('p: '))
+	process.append(PCB(at,bt,p))
+	if sat==-1:
+		sat=at
+	elif sat>at:
+		sat=at;
+
+
+for i in range(0,x):
+	for j in range(1,x-i):
+		
+		if process[j].p<process[j-1].p:
+			process[j],process[j-1]=process[j-1],process[j]
+
+cpu_cycle=sat;
+print('\n\n')
+final.append('p name\t TT \t BT')
+count=0
+while remain!=0:
+	
+
+	if process[count].at<=cpu_cycle and process[count].rt>0:
+		cpu_cycle=cpu_cycle+process[count].rt;
+		process[count].rt=0
+		process[count].cal(cpu_cycle)
+		remain=remain-1
+		final.append(process[count])
+		wt=wt+process[count].wt
+		tt=tt+process[count].tt
+		print(process[count].name+'|'+str(cpu_cycle)+'|',end='')
+	
+	count =count+1
+
+	if count==x:
+		count=0
+
+
+print('\n\n\n')
+final.append('avg: \t'+str(tt/x)+'\t'+str(wt/x)+'\t');
+
+for i in final:
+	print(i);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
